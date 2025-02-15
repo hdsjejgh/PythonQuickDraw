09b01e9b12281e05e62bc713db4377bb9c9e7948
hdsjejgh <aayanahmad08@hotmail.com>
2025-02-14 21:56:31 -0500

    Implemented basic model predictions thing. spent an hour debugging only to find out the image was inverted

--- NN.py
+++ NN.py
@@ -5,7 +5,8 @@ from sklearn.model_selection import train_test_split #just for the train test sp
 from progress.bar import Bar    #doesnt work??
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' #to get rid of the random warnings TF gives
 import tensorflow as tf
-
+from tensorflow.keras.regularizers import l2
+import PIL
 from time import time
 
 
@@ -18,7 +19,7 @@ def CreateModel():
     bar = Bar("Loading Data",max=numInputs)
     for idx,file in enumerate(os.listdir(url)): #loads all the data
         data = np.load(os.path.join(url,file))
-        for arr in data[0::100]:
+        for arr in data[0::33]:
             arr = arr/255
             arr = arr.reshape(28,28,1)
             datas.append(arr)
@@ -28,33 +29,41 @@ def CreateModel():
         bar.next()
     bar.finish()
     print("\nData Loaded")
-    print(len(datas))
+    #print(len(datas))
     #print(conversions)
-    input()
+    print(datas[1000].shape)
+    img = PIL.Image.fromarray(datas[0].reshape(28,28)*255)
+    img.show()
+
     labels = tf.keras.utils.to_categorical(labels, num_classes=numInputs)
 
     x_train,x_test,y_train,y_test = train_test_split(np.array(datas),np.array(labels),test_size=0.3)
-    """
+
     model = tf.keras.Sequential([
 
-        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+        tf.keras.layers.Conv2D(32, (5, 5), activation='relu', input_shape=(28, 28, 1)),
         tf.keras.layers.MaxPooling2D((2, 2)),
-        # Second Convolutional Layer
-        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_regularizer=l2(0.01)),
         tf.keras.layers.MaxPooling2D((2, 2)),
-        # Third Convolutional Layer (optional)
-        tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+
+        tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_regularizer=l2(0.01)),
         tf.keras.layers.MaxPooling2D((2, 2)),
-        # Flatten and Fully Connected Layers
+
         tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(128, activation='relu'),
         tf.keras.layers.Dropout(0.5),
+
+
         tf.keras.layers.Dense(numInputs, activation='softmax')
     ])
     model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
 
-    model.fit(x_train,y_train,epochs=10)
-    """
+    model.fit(x_train,y_train,epochs=7)
+
+    if input("Save? (Y/N): ").upper()=="Y":
+        model.save('model.keras')
+
     model = tf.keras.models.load_model('model.keras')
 
     y_pred = model.predict(x_test)
--- main.py
+++ main.py
@@ -1,34 +1,44 @@
+import os
 import tkinter as tk
-
-
+from PIL import Image
+import tensorflow as tf
+import numpy as np
 
 class QuickDrawApp:
     def __init__(self,time=20):
         self.conversion = {0: 'aircraft carrier', 1: 'banana', 2: 'barn', 3: 'basket', 4: 'bat', 5: 'bracelet', 6: 'bridge', 7: 'bucket', 8: 'cake', 9: 'cow', 10: 'door', 11: 'drill', 12: 'duck', 13: 'ear', 14: 'elbow', 15: 'envelope', 16: 'eraser', 17: 'face', 18: 'fence', 19: 'fire hydrant', 20: 'floor lamp', 21: 'foot', 22: 'fork', 23: 'garden hose', 24: 'garden', 25: 'golf club', 26: 'grass', 27: 'guitar', 28: 'hamburger', 29: 'hand', 30: 'harp', 31: 'headphones', 32: 'hexagon', 33: 'raccoon', 34: 'saxophone', 35: 'scissors', 36: 'shark', 37: 'snowflake', 38: 'squirrel', 39: 'sun', 40: 'sword', 41: 't-shirt', 42: 'telephone', 43: 'The Mona Lisa', 44: 'toaster', 45: 'traffic light', 46: 'trombone', 47: 'wine glass', 48: 'yoga', 49: 'zigzag'}
+        self.model = tf.keras.models.load_model('model.keras')
 
         self.root = tk.Tk()
+        self.mainframe = tk.Frame(self.root,background="#FFD139",width=256,height=550)
+        self.mainframe.pack()
 
-        self.title = tk.Label(self.root, text="Quick Draw",anchor="center", font=("MV Boli",25),bg="#FFD139")
+
+
+        self.title = tk.Label(self.mainframe, text="Quick Draw",anchor="center", font=("MV Boli",25),bg="#FFD139")
         self.title.place(relx=0.15)
 
         tk.Label(self.root, text="Your Word is:", anchor="center", font=("Ink Free", 15), bg="#FFD139").place(relx=0.28,y=50)
 
-        self.currentWord = tk.Label(self.root, text="PLACEHOLDER", anchor="center", font=("Ink Free", 18), bg="#FFD139")
+        self.currentWord = tk.Label(self.mainframe, text="PLACEHOLDER", anchor="center", font=("Ink Free", 18), bg="#FFD139")
         self.currentWord.place(relx=.50,y=100,anchor="center")
 
-        self.canvas = tk.Canvas(self.root, width=256, height=256, bg='white')
+        self.canvas = tk.Canvas(self.mainframe, width=256, height=256, bg='white')
         self.canvas.bind('<B1-Motion>', self.__draw)
         self.canvas.place(x=0, rely=0.25)
 
 
-        self.submit = tk.Button(self.root, text="Submit",font=("Ink Free",14),width=10,bg="#FFD139",relief='solid',command=self.__submit)
-        self.submit.place(relx=0.13,rely=0.75)
+        self.submit = tk.Button(self.mainframe, text="Submit",font=("Ink Free",12),width=10,bg="#FFD139",relief='solid',command=self.__submit)
+        self.submit.place(relx=0.1,rely=0.75)
+
+        self.clear = tk.Button(self.mainframe, text="X", font=("Ink Free", 14), width=1, bg="#FFD139",relief='solid', command=self.__clear)
+        self.clear.place(relx=0.58, rely=0.75)
 
         self.time=time
-        self.timer = tk.Label(self.root,text=self.time,font=("Ink Free",14),bg="#FFD139",relief="raised",width=4)
+        self.timer = tk.Label(self.mainframe,text=self.time,font=("Ink Free",14),bg="#FFD139",relief="raised",width=4)
         self.timer.place(relx=0.70,rely=0.76)
 
-        self.feedback = tk.Label(self.root, text="FEEDBACK", anchor="center", font=("Ink Free", 15), bg="#FFD139")
+        self.feedback = tk.Label(self.mainframe, text="FEEDBACK", anchor="center", font=("Ink Free", 15), bg="#FFD139")
         self.feedback.place(relx=.50,rely=0.9,anchor="center")
 
         self.root.geometry('256x550')
@@ -37,12 +47,28 @@ class QuickDrawApp:
         self.root.mainloop()
 
     def __submit(self):
-        pass
+        self.canvas.postscript(file='temp/img.eps')
+        with Image.open('temp/img.eps') as img:
+            img.load()
+            img = img.convert("L")
+            img = img.resize((28,28))
+
+            img.show()
+            img = np.array(img).reshape((1,28,28,1))
+            img = 255-img
+
+            print((np.array(img).reshape((1,28,28,1)))/255)
+            print(self.conversion[self.model.predict((img)/255).argmax()])
+        os.remove('temp/img.eps')
+
+    def __clear(self):
+        self.canvas.delete('all')
+
 
     def __draw(self,x):
         mouseX=x.x
         mouseY=x.y
-        self.canvas.create_oval(mouseX-5,mouseY-5,mouseX+5,mouseY+5,fill="black")
+        self.canvas.create_oval(mouseX-4,mouseY-4,mouseX+4,mouseY+4,fill="black")
         pass
 
 if __name__ == '__main__':
