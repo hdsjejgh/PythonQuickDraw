e5be7243f473f36ef459148e56b77ab12215f00b
hdsjejgh <aayanahmad08@hotmail.com>
2025-02-14 21:57:50 -0500

    fixed model

--- NN.py
+++ NN.py
@@ -41,16 +41,14 @@ def CreateModel():
 
     model = tf.keras.Sequential([
 
-        tf.keras.layers.Conv2D(32, (5, 5), activation='relu', input_shape=(28, 28, 1)),
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
         tf.keras.layers.MaxPooling2D((2, 2)),
 
-        tf.keras.layers.Conv2D(64, (3, 3), activation='relu', kernel_regularizer=l2(0.01)),
-        tf.keras.layers.MaxPooling2D((2, 2)),
-
-        tf.keras.layers.Conv2D(128, (3, 3), activation='relu', kernel_regularizer=l2(0.01)),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
         tf.keras.layers.MaxPooling2D((2, 2)),
 
         tf.keras.layers.Flatten(),
+
         tf.keras.layers.Dense(128, activation='relu'),
         tf.keras.layers.Dropout(0.5),
 
